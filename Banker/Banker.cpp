c7f741cb11dbb7317244de2e0218fa246aa60556
Oumourin <maolinwang1998@gmail.com>
2018-10-18 00:26:20 +0800

    更新设置二维数组方法

--- Banker/Banker.cpp
+++ Banker/Banker.cpp
@@ -19,48 +19,61 @@ void init(vector<int> &Available, vector<vector<int>> &Max, vector<vector<int>> 
 		Sum.push_back(temp);
 	}
 
-	for(int i=0;i<g_proNum;i++)		//设置每种进程所需的各种资源数量
+	for (int i = 0; i < g_proNum; i++)		//设置每种进程所需的各种资源数量
+	{
+		vector<int> maxTemp;
 		for (int j = 0; j < g_srcNum; j++)
 		{
 			int temp = 0;
 			cout << "输入第" << i + 1 << "进程的第" << j + 1 << "种资源所需要的最大数量:" << endl;
 			cin >> temp;
-			Max[i].push_back(temp);
+			maxTemp.push_back(temp);
 		}
-
+		Max.push_back(maxTemp);
+	}
 	for (int i = 0; i < g_proNum; i++)		//设置各个进程已获得的资源数
 	{
+		vector<int> allocationTemp;
 		for (int j = 0; j < g_srcNum; j++)
 		{
+			int temp;
 			cout << "输入第" << i + 1 << "进程的第" << j + 1 << "种资源已获得数量" << endl;
-			cin >> Allocation[i][j];
+			cin >> temp;
+			allocationTemp.push_back(temp);
 		}
+		Allocation.push_back(allocationTemp);
 	}
 	
 	for (int i = 0; i < g_srcNum; i++)		//计算空闲资源
 	{
+		int temp;
 		for (int j = 0; j < g_proNum; j++)
 		{
-			Available[i] = Max[j][i] - Allocation[j][i];
+			temp = Max[j][i] - Allocation[j][i];
+			Available.push_back(temp);
 		}
 	}
 
 	for (int i = 0; i < g_proNum; i++)		//计算各个进程还需要的资源数量
 	{
+		vector<int> needTemp;
+		int temp;
 		for (size_t j = 0; j < g_srcNum; j++)
 		{
-			Need[i][j] = Max[i][j] - Allocation[i][j];
+			temp = Max[i][j] - Allocation[i][j];
+			needTemp.push_back(temp);
 		}
+		Need.push_back(needTemp);
 	}
 
 	for (int i = 0; i < g_proNum; i++)		//初始化Finish标志
 	{
-		Finish[i] = false;
+		Finish.push_back(false);
 	}
 
 	for (int j = 0; j < g_srcNum; j++)		//初始化Request
 	{
-		Request[j] = 0;
+		Finish.push_back(0);
 	}
 }										
 
@@ -80,6 +93,7 @@ int safeCheck(vector<int> &Request, vector<vector<int>> &Need,vector<int> &Avail
 	//cout << "设置需要请求的进程的ID号:";
 	//cin >> proID;
 	setRequest(Request);
+	proID--;
 	for (size_t i = 0; i < g_srcNum; i++)
 	{
 		if (Request[i]>Need[proID][i])
@@ -205,7 +219,7 @@ void menu()
 	cout << "3、启动测试" << endl;
 	cout << "4、退出系统" << endl;
 	cout << "======================" << endl;
-	int proID;
+	int proID=1;
 	vector<int> Available;
 	vector<vector<int>> Max;
 	vector<vector<int>> Allocation;
@@ -219,7 +233,10 @@ void menu()
 	switch (flagCase)
 	{
 	case 1:init(Available, Max, Allocation, Need, Request, Finish, Sum); break;
-	case 2:cin>>proID; break;
+	case 2:
+	{
+		cin >> proID;	
+	}; break;
 	case 3: {
 		int flag;
 		flag = safeCheck(Request,Need,Available,proID);
